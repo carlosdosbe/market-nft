11ec2491c876dff0b845e84e48b810b7930b8348
Carlos Daniel OB <carlosdosbe@gmail.com>
2022-04-22 14:32:46 -0500

    (13)

---
+++ scripts/deploy.js
@@ -0,0 +1,16 @@
+
+const deploy = async () => {
+  const [deployer] = await ethers.getSigners();
+  console.log("Deploying contract with the account", deployer.address);
+  const MarketOS = await ethers.getContractFactory("MarketOS");
+  const deployed = await MarketOS.deploy();
+
+  console.log("MarketOS is deployed at: ", deployed.address);
+};
+
+deploy()
+  .then(() => process.exit(0))
+  .catch((error => {
+    console.log(error);
+    process.exit(1);
+}));
